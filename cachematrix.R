ff7834c1a9742697aa14a5c6d0f17218ecad9e84
Byron Stuart <trader.stuart@gmail.com>
2016-03-09 11:07:01 +1100

    coded cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,17 +9,17 @@
 ## set and get the inverse of the matrix;
 
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL
-	set <- function(y) {
-		x <<- y
-		inv <<- NULL
-	}
-	get <- function() x
-	setInverse <- function(inverse) inv <<- inverse
-	getInverse <- function() inv
-    list(set = set, get = get,
-         setInverse = setInverse,
-         getInverse = getInverse)
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+		        inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
